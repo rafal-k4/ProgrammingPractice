8b3decd40196d6ddd4cbbad64f49f303fea92592
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2019-11-22 13:30:58 +0100

    add First study case

---
+++ PracticeCase/StudyCases/JsonDeserializationAppsettingsFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    public class JsonDeserializationAppsettingsFile : IStudy
+    {
+        public void Execute()
+        {
+            
+        }
+    }
+}
