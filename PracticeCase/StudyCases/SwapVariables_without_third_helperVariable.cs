4776a361bd61f3e179fb2d7973b25d217f0d653c
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2019-11-29 15:24:45 +0100

    Add swap variable without third helper variable case

---
+++ PracticeCase/StudyCases/SwapVariables_without_third_helperVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class SwapVariables_without_third_helperVariable : IStudy
+    {
+        public void Execute()
+        {
+            int x = 2;
+            int y = 5;
+
+            Console.WriteLine($"{x} : {y}");
+
+            x ^= y;
+            y ^= x;
+            x ^= y;
+
+            Console.WriteLine($"{x} : {y}");
+        }
+    }
+}
