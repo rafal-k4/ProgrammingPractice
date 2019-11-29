fb4a0c9cf945f3b86f2b30568c818f40c54de84c
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2019-11-29 15:15:35 +0100

    Add string formatting using tostring method case

---
+++ PracticeCase/StudyCases/StringFormattingUsingToString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    internal class StringFormattingUsingToString : IStudy
+    {
+        public void Execute()
+        {
+            var number = 20.55123;
+
+            var formatted = number.ToString("0000");
+            var formatted2 = $"{number:##.###}";
+            var formatted3 = $"{number:###.###}";
+            var formatted4 = $"{number:#####.########}";
+
+            Console.WriteLine(formatted);
+            Console.WriteLine(formatted2);
+            Console.WriteLine(formatted3);
+            Console.WriteLine(formatted4);
+            ;
+        }
+    }
+}
