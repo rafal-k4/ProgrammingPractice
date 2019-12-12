ad05b440e2b5d0908be9b8ec3d52a79f58ae809c
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2019-12-12 13:03:07 +0100

    Accessing method in typescript outside the bundled by webpack file!

--- TypeScriptPractice/wwwroot/tsScripts/webpack.config.js
+++ TypeScriptPractice/wwwroot/tsScripts/webpack.config.js
@@ -17,5 +17,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'var',
+        library: 'EntryPoint'
     },
 };
