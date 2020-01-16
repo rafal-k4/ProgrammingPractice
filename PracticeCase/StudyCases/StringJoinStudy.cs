a59d0c48fc2163086aca7678369c76189d2bfe2f
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-01-16 10:11:08 +0100

    string.Join test study

---
+++ PracticeCase/StudyCases/StringJoinStudy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    public class StringJoinStudy : IStudy
+    {
+        public void Execute()
+        {
+            var listOfSomething = new List<string>();
+            var listOfSomething2 = new List<string> { "aa", "bb", "cc " };
+
+            var joinedString = string.Join(",", listOfSomething);
+            var joinedString2 = string.Join(",", listOfSomething2);
+
+            //result: when array is empty returned values is empty string
+
+        }
+    }
+}
