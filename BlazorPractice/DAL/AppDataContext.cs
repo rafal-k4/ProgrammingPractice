74b88b3050e15124bc591c4790940dae963d6f16
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-02-05 15:11:23 +0100

    add dal folder with dbcontext

---
+++ BlazorPractice/DAL/AppDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPractice.DAL
+{
+    public class AppDataContext: DbContext
+    {
+    }
+}
