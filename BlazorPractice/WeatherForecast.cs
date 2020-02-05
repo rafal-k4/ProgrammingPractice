dddeab64316894af5fcbe9ba09e2ddaef834157b
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-02-05 14:44:17 +0100

    moved code to component class

--- BlazorPractice/WeatherForecast.cs
+++ BlazorPractice/WeatherForecast.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorPractice.Models;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BlazorPractice
 {
     public class WeatherForecast : ComponentBase
     {
+        private HttpClient httpClient { get; set; }
 
+        public WeatherForecast(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<WeatherForecastModel>> GetWeatherForecst()
+        {
+            return await httpClient.GetJsonAsync<IEnumerable<WeatherForecastModel>>("sample-data/weather.json");
+        }
     }
 }
