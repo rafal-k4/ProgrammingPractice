e8ffd667d1a6d254c7b57800ef04e0ef8c1a03cf
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-02-05 15:48:26 +0100

    registered dbcontext

--- BlazorPractice/Program.cs
+++ BlazorPractice/Program.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using BlazorPractice.DAL.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorPractice
 {
@@ -12,7 +14,16 @@ namespace BlazorPractice
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddDbContext<AppDataContext>(options =>
+                options.UseSqlServer("Server=.\\MSSQLDEV;Database=BasicDataCheck;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
+
             builder.RootComponents.Add<App>("app");
+
+            //var host = builder.Build();
+            //host.Configuration
+            
 
             await builder.Build().RunAsync();
         }
