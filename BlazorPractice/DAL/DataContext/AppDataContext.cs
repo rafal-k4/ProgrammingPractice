1b2f9d9341a4c577dba83fbdd80cb90b86a2fce2
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-02-05 15:16:40 +0100

    moved dbcontext to a datacontext folder

--- BlazorPractice/DAL/DataContext/AppDataContext.cs
+++ BlazorPractice/DAL/DataContext/AppDataContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlazorPractice.DAL
+namespace BlazorPractice.DAL.DataContext
 {
     public class AppDataContext: DbContext
     {
