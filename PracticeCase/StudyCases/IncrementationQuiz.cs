0d87662ef51ee64451a64ebdcaa3d23df871a56e
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-02-26 16:45:15 +0100

    Add IncremetingQuiz study case

---
+++ PracticeCase/StudyCases/IncrementationQuiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    public class IncrementationQuiz : IStudy
+    {
+        public void Execute()
+        {
+            int a = 2;
+            int a2 = 2;
+            int a3 = 2;
+
+            var b = a++ + ++a; //6
+            var b2 = ++a2 + ++a2; //7
+            var b3 = a3++ + a3++; //5
+            ;
+        }
+    }
+}
