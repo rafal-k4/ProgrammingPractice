d08972e9ae1798f7bb219a8a45cbc02f1d8c1300
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-03-06 09:22:49 +0100

    add comparing date with .date property and without study case

---
+++ PracticeCase/StudyCases/ComparingOnlyDateUsingDatetime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    public class ComparingOnlyDateUsingDatetime : IStudy
+    {
+        public void Execute()
+        {
+            var date1 = DateTime.Now;
+            var date2 = DateTime.Now.AddMinutes(10);
+
+            var compare1 = date1 == date2; // false
+            var compare2 = date1.Date == date2.Date; // true
+
+            ;
+        }
+    }
+}
