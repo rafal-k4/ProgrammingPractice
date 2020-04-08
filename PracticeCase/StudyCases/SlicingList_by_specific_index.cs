59b30d425067a3b92bac44911d55e221c64ac682
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-04-08 11:15:59 +0200

    add slicing list using List<T>.GetRange() method

---
+++ PracticeCase/StudyCases/SlicingList_by_specific_index.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class SlicingList_by_specific_index : IStudy
+    {
+        public void Execute()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+
+            var sliceIndex = 3;
+
+            // expected: one list of { 1, 2, 3} and other of { 4, 5, 6, 7 }
+
+            var firstSlicedList = list.GetRange(0, sliceIndex);
+            var secondSlicedList = list.GetRange(sliceIndex, list.Count - sliceIndex);
+            ;
+
+        }
+    }
+}
