4515104afeae1b6bf6ab78c86ea14bfd1ab06cc0
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-04-17 14:36:36 +0200

    add Intersect and Except study case using EqualityComparer!

---
+++ PracticeCase/StudyCases/IntersectExcept_Using_EqualityComparer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utils;
+
+namespace PracticeCase.StudyCases
+{
+    class IntersectExcept_Using_EqualityComparer : IStudy
+    {
+        public void Execute()
+        {
+            var firstList = new List<Person>
+            {
+                new Person("a", 1),
+                new Person("b", 2),
+                new Person("c", 3)
+            };
+
+            var secondList = new List<Person>
+            {
+                new Person("b", 2),
+                new Person("c", 3),
+                new Person("d", 4)
+            };
+
+
+            var resultOfIntersect = firstList.Intersect(
+                secondList,
+                EqualityComparer.Get<Person>((x, y) => x.Name == y.Name)).ToList(); // { b, 2 }, { c, 3 } 
+
+            var resultOfExcept = firstList.Except(
+                secondList,
+                EqualityComparer.Get<Person>((x, y) => x.Name == y.Name)).ToList(); // { a, 1 }
+
+            ;
+        }
+    }
+
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
---
+++ Utils/EqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public class EqualityComparer
+    {
+        // This comparer comapres only by defined func! (GetHashCode always returns 0)
+        public static EqualityComparer<U> Get<U>(Func<U, U, bool> func)
+        {
+            return new EqualityComparer<U>(func);
+        }
+    }
+
+    public class EqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> comparisonFunction;
+        public EqualityComparer(Func<T, T, bool> func)
+        {
+            comparisonFunction = func;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return comparisonFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return 0;
+        }
+    }
+}
