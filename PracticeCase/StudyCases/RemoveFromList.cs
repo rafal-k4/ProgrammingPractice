bf345738e17e8f35a0524838f75991e6036b2906
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2020-05-11 13:58:55 +0200

    RemoveFromList study case

---
+++ PracticeCase/StudyCases/RemoveFromList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class RemoveFromList : IStudy
+    {
+        public void Execute()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+
+            list.RemoveRange(0, 3); // 4, 5
+            ;
+        }
+    }
+}
