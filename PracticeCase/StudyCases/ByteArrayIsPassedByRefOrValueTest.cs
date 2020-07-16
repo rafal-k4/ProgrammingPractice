caef5fe448d93df259f5e24cfbd3929cea7706a4
kapuscinski_rafal <rafal_kapuscinski@next.co.uk>
2020-07-16 16:29:54 +0200

    add trivial case, but had to test because of some article(author passed byte array as ref - only modyfing content)

---
+++ PracticeCase/StudyCases/ByteArrayIsPassedByRefOrValueTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    public class ByteArrayIsPassedByRefOrValueTest : IStudy
+    {
+        public void Execute()
+        {
+            var bytes = new byte[] {1, 2, 3};
+
+            ModifySomeBytes(bytes);
+
+            Console.WriteLine(string.Join(" | ", bytes)); // 2 | 4 | 6
+
+            ModifySomeBytesByRef(ref bytes);
+
+            Console.WriteLine(string.Join(" | ", bytes)); // 4 | 8 | 12
+            ;
+        }
+
+        private void ModifySomeBytesByRef(ref byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] += bytes[i];
+            }
+        }
+
+        private void ModifySomeBytes(byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] += bytes[i];
+            }
+        }
+    }
+}
