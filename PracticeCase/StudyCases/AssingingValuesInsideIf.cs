f41ac6d0fde464471b2b626b3bd0c82639cdb904
kapuscinski_rafal <rafal_kapuscinski@next.co.uk>
2020-08-04 08:52:00 +0200

    Add ToDictionary with repeatable elements -> throws exception as expected

---
+++ PracticeCase/StudyCases/AssingingValuesInsideIf.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class AssingingValuesInsideIf : IStudy
+    {
+        public void Execute()
+        {
+            bool booleanVariable = true;
+
+            if (booleanVariable = false)
+            {
+                Console.WriteLine("Hello from IF");
+            }
+            else
+            {
+                Console.WriteLine("Hello from ELSE");
+            }
+
+            bool booleanVariable2 = false;
+
+            if (booleanVariable2 = true)
+            {
+                Console.WriteLine("Hello from IF");
+            }
+            else
+            {
+                Console.WriteLine("Hello from ELSE");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PracticeCase/StudyCases/ToDictionaryFromIEnumerable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class ToDictionaryFromIEnumerable : IStudy
+    {
+        public void Execute()
+        {
+            IEnumerable<string> stringListWithRepeteadElements = new List<string> {"aa", "bb", "aa"};
+
+            var dictionary2 = stringListWithRepeteadElements.Distinct().ToDictionary(x => x, x => x + ".value");
+
+            var dictionary = stringListWithRepeteadElements.ToDictionary(x => x, x => x + ".value"); // exception
+
+            ;
+        }
+    }
+}
