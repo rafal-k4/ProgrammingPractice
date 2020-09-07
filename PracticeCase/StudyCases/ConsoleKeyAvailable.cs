1ec156fbe4ab0b460da9a2b207854efee893418d
kapuscinski_rafal <rafal_kapuscinski@next.co.uk>
2020-09-07 15:39:37 +0200

    add Console.KeyAvailable study case

---
+++ PracticeCase/StudyCases/ConsoleKeyAvailable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace PracticeCase.StudyCases
+{
+    class ConsoleKeyAvailable : IStudy
+    {
+        public void Execute()
+        {
+            //Console.ReadKey(true); // This reset KeyAvailable to false and doesn't display pressed key
+            //Console.ReadKey(); // This reset KeyAvailable to false
+            Console.ReadLine(); // This reset KeyAvailable to false
+
+            Console.WriteLine("Press any key to break"); // any key press makes KeyAvailable to true!
+
+
+            while (Console.KeyAvailable == false)
+            {
+                Console.WriteLine("Doing something!");
+                Task.Delay(800).GetAwaiter().GetResult();
+            }
+
+        }
+    }
+}
