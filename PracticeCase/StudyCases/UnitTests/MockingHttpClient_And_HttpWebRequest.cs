072037a818fa639ce292389c941ce1ee3cbd0c7e
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2021-04-23 14:53:30 +0200

    Add UnitTest-based study cases -> Mocking Http Client and mocking HttpWebRequest Add ValidationJson by model generated schema

---
+++ PracticeCase/StudyCases/UnitTests/MockingHttpClient_And_HttpWebRequest.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PracticeCase.StudyCases.UnitTests
+{
+    class MockingHttpClient_And_HttpWebRequest : IStudy
+    {
+        public void Execute()
+        {
+            var responseMessage = "test";
+            var mockedResponse = WayToMock_HttpWebRequest(responseMessage);
+
+            var mockedHttpClient = WayToMock_HttpClient(responseMessage);
+            var responseFromHttpClient = mockedHttpClient.GetAsync("http://test.com").GetAwaiter().GetResult().Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            Debug.Assert(responseMessage == mockedResponse);
+            Debug.Assert(responseMessage == responseFromHttpClient);
+        }
+
+        private string WayToMock_HttpWebRequest(string responseMessage)
+        {
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+            streamWriter.Write(responseMessage);
+            streamWriter.Flush();
+            memoryStream.Position = 0;
+
+
+            var httpWebResponseMock = new Mock<HttpWebResponse>();
+            httpWebResponseMock.Setup(x => x.GetResponseStream()).Returns(memoryStream);
+            httpWebResponseMock.Setup(x => x.StatusCode).Returns(HttpStatusCode.OK);
+
+            var httpWebRequestMock = new Mock<HttpWebRequest>();
+            httpWebRequestMock.Setup(x => x.GetResponse()).Returns(httpWebResponseMock.Object);
+
+
+            var resultStream = httpWebRequestMock.Object.GetResponse().GetResponseStream();
+
+            using (StreamReader streamReader = new StreamReader(resultStream))
+            {
+                var result = streamReader.ReadToEnd();
+                return result;
+            }
+        }
+
+        private HttpClient WayToMock_HttpClient(string responseMessage)
+        {
+            // how to mock HttpCLient article: https://gingter.org/2018/07/26/how-to-mock-httpclient-in-your-net-c-unit-tests/
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                // Setup the PROTECTED method to mock
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                // prepare the expected response of the mocked http call
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(responseMessage)
+                })
+                .Verifiable();
+
+            return new HttpClient(handlerMock.Object);
+        }
+    }
+}
---
+++ PracticeCase/StudyCases/UnitTests/ValidatingJson_By_ModelSchema.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PracticeCase.StudyCases.UnitTests
+{
+    class ValidatingJson_By_ModelSchema : IStudy
+    {
+        /// <summary>
+        /// This method takes schema based on the model with specified JsonProperty, compares it with example response in Json format
+        /// and then validates it.
+        /// As a summary possible exception is thrown with details about missing / additional properties
+        /// </summary>
+        public void Execute()
+        {
+
+            JSchemaGenerator generator = new JSchemaGenerator();
+            generator.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            JObject response = JObject.Parse(_response);
+            
+
+            JSchema schema = generator.Generate(typeof(JsonModel));
+
+
+            //schema.AllowAdditionalProperties = false;
+
+            RejectAdditionalProperties(schema);
+            if (!response.IsValid(schema, out IList<string> messages))
+            {
+                // In proper unit test project better throw more specified execption -> AssertionFailExeception
+                throw new Exception(string.Join(Environment.NewLine, messages));
+            }
+            ;
+        }
+
+        public void RejectAdditionalProperties(JSchema schema)
+        {
+            schema.AllowAdditionalProperties = false;
+            foreach (var s in schema.Properties.Values) RejectAdditionalProperties(s);
+        }
+
+        private static string _response =
+            "{" +
+            "   \"fullName\" : \"My Full Name\", " +
+            "   \"name\" : \"My Name\", " +
+            "   \"nestedProp\" : { " +
+            "       \"age\" : 10, " +
+            "       \"number\" : 20, " +
+            "   }" +
+            "}";
+
+        private class JsonModel
+        {
+            public string Name { get; set; }
+            [JsonProperty("nestedProp")]
+            public JsonNestedModel Nested { get; set; }
+
+            public class JsonNestedModel
+            {
+                public int Age { get; set; }
+            }
+        }
+
+    }
+
+    
+
+
+}
