998a49df9c4444dbdcbf918ce764793a98bb6406
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2021-05-23 20:06:18 +0200

    add closure case study

---
+++ PracticeCase/StudyCases/ClosureFun.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCase.StudyCases
+{
+    class ClosureFun : IStudy
+    {
+        public void Execute()
+        {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+
+            var actions = new List<Action>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                actions.Add(() => Console.WriteLine($"value of array element is: {i}"));
+            }
+
+            actions.ForEach(x => x());
+            // ----RESULT----- 
+            // value of array element is: 5
+            // value of array element is: 5
+            // value of array element is: 5
+            // value of array element is: 5
+            // value of array element is: 5
+
+            actions = new List<Action>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var tempVariableForNonClosure = i;
+                actions.Add(() => Console.WriteLine($"value of array element is: {tempVariableForNonClosure}"));
+            }
+
+            actions.ForEach(x => x());
+            // ----RESULT----- 
+            // value of array element is: 0
+            // value of array element is: 1
+            // value of array element is: 2
+            // value of array element is: 3
+            // value of array element is: 4
+
+            ;
+        }
+    }
+}
