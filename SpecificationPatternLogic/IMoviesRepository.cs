e710b81303ce7b9275783bc5c621fea570ff24ca
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2021-12-19 11:55:39 +0100

    extracted logic to expression that are in client code

--- SpecificationPatternLogic/IMoviesRepository.cs
+++ SpecificationPatternLogic/IMoviesRepository.cs
@@ -1,13 +1,13 @@
 ﻿using SpecificationPatternLogic.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace SpecificationPatternLogic
 {
     public interface IMoviesRepository
     {
-        IReadOnlyList<Movie> GetMovies(bool forKidsOnly, bool cdAvailable, int minRating);
+        IReadOnlyList<Movie> GetMovies(Expression<Func<Movie, bool>> predicate, int minRating);
         Movie GetById(long movieId);
-        bool IsForChildOnly(Movie movie);
-        bool IsCdAvailable(Movie movie);
     }
 }
--- SpecificationPatternLogic/MoviesRepository.cs
+++ SpecificationPatternLogic/MoviesRepository.cs
@@ -4,6 +4,7 @@ using SpecificationPatternLogic.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace SpecificationPatternLogic
 {
@@ -15,27 +16,15 @@ namespace SpecificationPatternLogic
             this.dbContext = dbContext;
         }
 
-        public bool IsForChildOnly(Movie movie)
-        {
-            return movie.MpaaRating <= MpaaRating.PG;
-        }
-
-        public bool IsCdAvailable(Movie movie)
-        {
-            return movie.ReleaseDate <= DateTime.Now.AddMonths(-6);
-        }
-
         public Movie GetById(long movieId)
         {
             return dbContext.Movies.First(x => x.MovieId == movieId);
         }
 
-        public IReadOnlyList<Movie> GetMovies(bool forKidsOnly, bool cdAvailable, int minRating)
+        public IReadOnlyList<Movie> GetMovies(Expression<Func<Movie, bool>> predicate, int minRating)
         {
             return dbContext.Movies
-                // solution .ToList() ??
-                .Where(x => IsForChildOnly(x) || !forKidsOnly)
-                .Where(x => IsCdAvailable(x) || !cdAvailable)
+                .Where(predicate)
                 .Where(x => x.Rating >= minRating)
                 .Include(x => x.Director)
                 .ToList();
